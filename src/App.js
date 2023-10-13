e56b77653691be2771f900bdc1a54e9e7b812b4c
mohammadkhadeer123 <47175107+mohammadkhadeer123@users.noreply.github.com>
2023-10-13 18:29:32 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import "./styles.css";
+
+export default function App() {
+  const [data, setData] = useState([
+    { id: 1, name: "John", age: 25 },
+    { id: 2, name: "Jane", age: 30 },
+    { id: 3, name: "Bob", age: 35 }
+  ]);
+  return (
+    <div className="App">
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Age</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item) => (
+            <tr key={item.id}>
+              <td>{item.id}</td>
+              <td>{item.name}</td>
+              <td>{item.age}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+import React, { useState } from "react";
